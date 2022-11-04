b628e541b899a82eb6d05e5657149c16491844de
Talta <68383301+TaltaM@users.noreply.github.com>
2022-11-04 12:51:36 +0100

    Add scavenge messages

--- ts-client/scavenge.scavenge/registry.ts
+++ ts-client/scavenge.scavenge/registry.ts
@@ -1,6 +1,12 @@
 import { GeneratedType } from "@cosmjs/proto-signing";
+import { MsgSubmitScavenge } from "./types/scavenge/scavenge/tx";
+import { MsgRevealSolution } from "./types/scavenge/scavenge/tx";
+import { MsgCommitSolution } from "./types/scavenge/scavenge/tx";
 
 const msgTypes: Array<[string, GeneratedType]>  = [
+    ["/scavenge.scavenge.MsgSubmitScavenge", MsgSubmitScavenge],
+    ["/scavenge.scavenge.MsgRevealSolution", MsgRevealSolution],
+    ["/scavenge.scavenge.MsgCommitSolution", MsgCommitSolution],
     
 ];
 
